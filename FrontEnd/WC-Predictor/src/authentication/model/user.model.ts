6633403b6036f5aeadb9fd76a5ad35186d9ab5fd
Dominic Azizi <Dominic@dhcp-206-87-208-147.ubcsecure.wireless.ubc.ca>
2018-03-12 11:49:00 -0700

    forgot some files

---
+++ FrontEnd/WC-Predictor/src/authentication/model/user.model.ts
@@ -0,0 +1,17 @@
+import { DataModel } from "../../shared/datamodel/datamodel";
+import { environment } from "../../environments/environment";
+
+export class User extends DataModel{ 
+    id: string = "";
+    email: string = "";
+    username: string = "";
+    password: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    dateCreated: Date = null;
+    dateUpdated: Date = null;
+
+    getMetaData(): string {
+        return environment.LOCAL +  "/user";
+    }
+}
