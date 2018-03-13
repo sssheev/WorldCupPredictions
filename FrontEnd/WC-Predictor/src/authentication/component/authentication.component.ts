20bdf6e4f42ec85384d644196f27494c05edd34c
Dominic Azizi <Dominic@Dominics-MacBook-Pro.local>
2018-03-12 18:04:56 -0700

    navigating between login and register

---
+++ FrontEnd/WC-Predictor/src/authentication/component/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { User } from '../model/user.model';
+
+@Component({
+  selector: 'authentication-box',
+  templateUrl: './authentication.component.html'
+})
+export class AuthenticationComponent {
+  pageNumber =1;
+
+}
